6fcf26d3203b347f790feedca41cdf0e8f5385c2
Mamadou NIANG <contact.stateless@gmail.com>
2022-08-11 11:14:42 +0000

    ajouter les access api au projet angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '90d26a866598b3fc62c75bfcb8068823',
+  apiUrl: 'https://api.themoviedb.org/3',
+  imgUrl: 'https://image.tmdb.org/t/p/original',
 };
 
 /*
