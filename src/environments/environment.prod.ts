75a5df35c05b42749bfe992c255113db2efcc317
ryuzvki-sn <contact.stateless@gmail.com>
2022-10-25 18:15:53 +0000

    add ghpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '90d26a866598b3fc62c75bfcb8068823',
+  apiUrl: 'https://api.themoviedb.org/3',
+  imgUrl: 'https://image.tmdb.org/t/p/original',
 };
